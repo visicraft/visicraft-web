d91f4fcf044ffc34fa9a53d4af7e98404dfb1ff2
novacbn <31122674+novacbn@users.noreply.github.com>
2019-11-14 04:38:12 -0500

    - Added `visicraft.css` import back, apparently didn't notice it disappearing - Changed adapter to `memory` - Updated `visicraft-lib` build

--- src/client.js
+++ src/client.js
@@ -4,13 +4,15 @@ import "luda/dist/js/luda";
 
 import * as sapper from "@sapper/app";
 
+import "./visicraft.css";
+
 const target = document.querySelector("#sapper");
 target.innerHTML = "LOADING, PLEASE WAIT...";
 
 (async function() {
     window.client = new visicraft.VisicraftClient({
         datastore: {
-            adapter: "idb"
+            adapter: "memory"
         }
     });
 
