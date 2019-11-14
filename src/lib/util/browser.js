c9923ebeabeba27a55678885769f505305eba85c
novacbn <31122674+novacbn@users.noreply.github.com>
2019-11-14 08:24:52 -0500

    - Updated `src/route/editor/races/index.svelte` to persist search parameters to URL

---
+++ src/lib/util/browser.js
@@ -0,0 +1,27 @@
+export function get_query_string() {
+    const search = new URLSearchParams(location.search);
+    const ret = {};
+
+    for (const [key, value] of search.entries()) ret[key] = value;
+    return ret;
+}
+
+/**
+ * Updates the current URL query string of the Browser, with the given object<string, string>
+ * @param {*} query_params
+ */
+export function update_query_string(query_params) {
+    let search = new URLSearchParams(location.search);
+
+    for (const key in query_params) {
+        const value = query_params[key];
+
+        if (typeof value === "undefined") search.delete(key);
+        else search.set(key, value.toString());
+    }
+
+    search = search.toString();
+    const url = search ? `${location.pathname}?${search}` : location.pathname;
+
+    history.replaceState(null, "", url);
+}
--- src/routes/editor/races/index.svelte
+++ src/routes/editor/races/index.svelte
@@ -5,14 +5,32 @@
     import SearchGrid from "../../../components/patterns/searchgrid/SearchGrid.svelte";
 
     import {get_races} from "../../../lib/stores/races";
+    import {get_query_string, update_query_string} from "../../../lib/util/browser";
     import {SORTING_DIRECTIONS, SORTING_MODES} from "../../../lib/util/constants";
 
+    const query_params = get_query_string();
+
     let column_size;
-    let filter = "";
-    let sorting_direction = SORTING_DIRECTIONS.ascending;
-    let sorting_mode = SORTING_MODES.recent;
+    let filter = query_params.filter ? query_params.filter : "";
+    let sorting_direction = query_params.sorting_direction
+        ? query_params.sorting_direction
+        : SORTING_DIRECTIONS.ascending;
+    let sorting_mode = query_params.sorting_mode ? query_params.sorting_mode : SORTING_MODES.recent;
 
     $: races = get_races(sorting_direction, sorting_mode, filter);
+
+    $: {
+        const _filter = filter ? filter : undefined;
+        const _sorting_direction =
+            sorting_direction === SORTING_DIRECTIONS.ascending ? undefined : sorting_direction;
+        const _sorting_mode = sorting_mode === SORTING_MODES.recent ? undefined : sorting_mode;
+
+        update_query_string({
+            filter: _filter,
+            sorting_direction: _sorting_direction,
+            sorting_mode: _sorting_mode
+        });
+    }
 </script>
 
 <svelte:head>
